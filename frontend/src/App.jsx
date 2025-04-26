2ce6dd1652a1e44f3f41c168539c177d651e0219
Saravinda Sarathchandra <155600672+sara123-ms@users.noreply.github.com>
2025-04-26 11:36:08 +0530

    Update App.jsx with token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,15 @@ import Profile from "./components/Profile";
 import Post from "./components/Post";
 import Comments from "./components/Comments";
 import Posts from "./components/Posts";
+import axios from 'axios';
 
 function App() {
+  
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW51amFzYW5kZWVwQGdtYWlsLmNvbSIsImlhdCI6MTc0NTYzNTA0NSwiZXhwIjoxNzQ1NjcxMDQ1fQ.4WxmSx_K50hdrDdwdU0uQPco0v_edMpKE-ITgHGpRgI";
+
+  
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  axios.defaults.baseURL = 'http://localhost:8080'; 
   return (
     <BrowserRouter>
       {/* <Header /> */}
